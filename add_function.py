ff868a14363886131f196dd03cf13fa9c4713c4c
Joshua Mora <joshua_mora@usa.net>
2022-09-05 21:32:41 -0500

    function content for add and multiply
    added functions add and multiply

--- add_function.py
+++ add_function.py
@@ -1,1 +1,14 @@
+def add(a,b):
+    c=a+b
+    return c
+
+def main():
+    
+    a=3
+    b=4
+    c=add(a, b)
+    print("A= ",a," B= ",b,"C= ",c)
+
+if __name__ == "__main__":
+    main()
 
--- multiply_function.py
+++ multiply_function.py
@@ -1,1 +1,15 @@
+def multiply(a,b):
+    c=a*b
+    return c
+
+def main():
+    
+    a=3
+    b=4
+    c=multiply(a, b)
+    print("A= ",a," B= ",b,"C= ",c)
+
+if __name__ == "__main__":
+    main()
+
 
